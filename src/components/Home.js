60f34f0ee849ed3e3686d39c1cd69885d50759cc
Wen Loong Ng <wenloongng@gmail.com>
2019-08-12 04:35:01 -0700

    Updated home

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,11 +10,22 @@ class Home extends Component {
       return (
          <div className="home-wrapper">
             <div className="home-elements-wrapper">
-               <div className="greeting-wrapper">
-                  <h1>Bonjour</h1>
+               <div className="home-left-wrapper">
+                  <div className="greeting-wrapper">
+                     <h1>Bonjour!</h1>
+                  </div>
+                  <div className="profile-image-wrapper">
+                     <img src={profile}></img>
+                  </div>
                </div>
-               <div className="profile-image-wrapper">
-                  <img src={profile}></img>
+
+               <div className="home-right-wrapper">
+                  <div className="desc-wrapper">
+                     <p>My name is Clifford Ng. I am a Computer Science and
+                        Engineering student with great interest in web development,
+                        artificial intelligence, and machine learning.
+                     </p>
+                  </div>
                </div>
             </div>
          </div>
