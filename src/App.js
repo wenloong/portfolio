fcad978f1bc1ab7400ea5d400f10f15b9551e592
Wen Loong Ng <wenloongng@gmail.com>
2019-08-12 04:33:55 -0700

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 //Stylesheets
 import './stylesheets/Normalize.css';
@@ -6,13 +7,17 @@ import './stylesheets/Normalize.css';
 //Importing Components
 import Navigation from './components/Navigation';
 import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <Navigation/>
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation/>
+        <Route path="/Home" component={Home} />
+        <Route path="/About" component={About}/>
+      </div>
+    </Router>
   );
 }
 
