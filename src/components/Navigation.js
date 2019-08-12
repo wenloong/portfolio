e154730c27b5352914fdae8c672ac72ca4acc36f
Wen Loong Ng <wenloongng@gmail.com>
2019-08-12 04:34:33 -0700

    Updated navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
 import '../stylesheets/Normalize.css';
 import '../stylesheets/Navigation.css';
 
 class Navigation extends Component {
    render() {
       return (
-         <div className="navigation-menu">
-            <ul>
-               <li><a href="#">Home</a></li>
-               <li><a href="#">Projects</a></li>
-               <li><a href="#">Skills</a></li>
-               <li><a href="#">About</a></li>
-            </ul>
+         <div className="navigation-wrapper">
+            <div className="navigation-menu">
+               <div className="navigation-menu-ul-wrapper">
+                  <ul>
+                     <li><Link to={"/Home"}>Home</Link></li>
+                     <li><Link to={"/#"}>Projects</Link></li>
+                     <li><Link to={"/#"}>Skills</Link></li>
+                     <li><Link to={"/About"}>About</Link></li>
+                  </ul>
+               </div>
+            </div>
          </div>
       )
    }
